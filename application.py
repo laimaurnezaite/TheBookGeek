4b394cf961152d9878152c5ce74d5c515b54f336
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-03-16 10:37:03 +0200

    add search route

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -43,20 +43,24 @@ conn.commit()
 # engine = create_engine(os.getenv("DATABASE_URL"))
 # db = scoped_session(sessionmaker(bind=engine))
 
-# # create tables
-# db.execute("CREATE TABLE IF NOT EXISTS books (isbn VARCHAR(255) NOT NULL PRIMARY KEY, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, year INTEGER NOT NULL)")
-# db.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, hash VARCHAR(255) NOT NULL)")
-# db.execute("CREATE TABLE IF NOT EXISTS reviews (user_id INTEGER, book_isbn VARCHAR(255), review VARCHAR NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (book_isbn) REFERENCES books (isbn))")
-        
-
 # # commit the changes
 # db.session.commit()
 
  
- 
-
-
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/form/search", methods=["GET"])
+def render_search():
+    return render_template("search.html")
+
+@app.route("/search", methods=["POST"])
+def search():
+    # cur = conn.cursor()
+    keyword = request.form.get("search").capitalize()
+    cur.execute(f"SELECT * FROM books WHERE author LIKE '%{keyword}%' OR title LIKE '%{keyword}%' OR isbn LIKE '%{keyword}%'")
+    rows = cur.fetchall()
+    return render_template("book.html", rows=rows)
+    
