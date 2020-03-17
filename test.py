3864b9f66eb8b9f0466b4d4c1149db1b2eb3afd6
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-03-17 11:46:20 +0200

    testing some stuff

--- test.py
+++ test.py
@@ -6,17 +6,21 @@ isbn = '0060852577'
 # res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": api_key, "isbns": isbn})
 # print(res.json())
 
-response = {'books': [{
-    'id': 6433752, 
-    'isbn': '0060852577', 
-    'isbn13': '9780060852573', 
-    'ratings_count': 50990, 
-    'reviews_count': 87859, 
-    'text_reviews_count': 6964, 
-    'work_ratings_count': 56496, 
-    'work_reviews_count': 98041, 
-    'work_text_reviews_count': 7625, 
-    'average_rating': '3.79'
-    }]
-}
-print(response["books"][0]["average_rating"])
+# response = {'books': [{
+#     'id': 6433752, 
+#     'isbn': '0060852577', 
+#     'isbn13': '9780060852573', 
+#     'ratings_count': 50990, 
+#     'reviews_count': 87859, 
+#     'text_reviews_count': 6964, 
+#     'work_ratings_count': 56496, 
+#     'work_reviews_count': 98041, 
+#     'work_text_reviews_count': 7625, 
+#     'average_rating': '3.79'
+#     }]
+# }
+# print(response["books"][0]["average_rating"])
+
+
+response = {"title": "The Bone Bed", "author": "Patricia Cornwell", "year": 2012, "isbn": "0399157565", "review_count": 20433, "average_score": "3.68"}
+print(len(response))
