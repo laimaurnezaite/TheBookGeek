00b94e54e0c5548a6087cb11bb74bedf6e1f40b0
Laima Urnezaite <laima.urnezaite@gmail.com>
2020-03-18 11:59:07 +0200

    create import file (data from csv to database)

--- import.py
+++ import.py
@@ -2,22 +2,35 @@ import csv
 import os
 
 from flask import Flask, render_template, request
-from models import *
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
 
 def main():
     csv_file = open("books.csv")
     rows = csv.DictReader(csv_file)
-    print(rows)
     for row in rows:
-        book = Book(isbn=row["isbn"], title=row["title"], author=row["author"], year=row["year"])
-        db.session.add(book)
+        # print(row)
+        db.execute("""CREATE TABLE IF NOT EXISTS books (isbn VARCHAR(255) NOT NULL PRIMARY KEY, title VARCHAR(255) NOT NULL, author VARCHAR(255) NOT NULL, year INTEGER NOT NULL)""")
+        db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:isbn, :title, :author, :year)", {"isbn":row['isbn'], "title":row["title"], "author":row["author"], "year":row["year"]})
         print(f'Added book: isbn: {row["isbn"]}, {row["title"]} by {row["author"]}, written in {row["year"]}')
-    db.session.commit()
+    db.commit()
+    print("added all books from csv file")
 
 if __name__ == "__main__":
     with app.app_context():
